37df05c00d0ca648b4d0470d859361c179b2f992
jiji-hoon96 <94469974+jiji-hoon96@users.noreply.github.com>
2022-05-22 15:23:52 +0900

    3. styled-components를 이용해 style

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,17 +3,25 @@ import { useRouter } from "next/router";
 
 export default function NavBar() {
   const router = useRouter();
-
   return (
     <nav>
       <Link href="/">
-        <a style={{ color: router.pathname === "/" ? "red" : "blue" }}>Home</a>
+        <a className={router.pathname === "/" ? "active" : ""}>Home</a>
       </Link>
       <Link href="/about">
-        <a style={{ color: router.pathname === "/about" ? "red" : "blue" }}>
-          About
-        </a>
+        <a className={router.pathname === "/about" ? "active" : ""}>About</a>
       </Link>
+      <style jsx>{`
+        nav {
+          background-color: tomato;
+        }
+        a {
+          text-decoration: none;
+        }
+        .active {
+          color: yellow;
+        }
+      `}</style>
     </nav>
   );
 }
