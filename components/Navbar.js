bd386959e134ac8e1ea46538e487e6a9489c4f76
jiji-hoon96 <94469974+jiji-hoon96@users.noreply.github.com>
2022-05-23 14:26:08 +0900

    _app.js 를이용해 전역으로 style

--- components/Navbar.js
+++ components/Navbar.js
@@ -13,13 +13,13 @@ export default function NavBar() {
       </Link>
       <style jsx>{`
         nav {
-          background-color: tomato;
+          background-color: white;
         }
         a {
           text-decoration: none;
         }
         .active {
-          color: yellow;
+          color: tomato;
         }
       `}</style>
     </nav>
