2dea69ce324cefce50a3084868f252c9dd23ace8
jiji-hoon96 <94469974+jiji-hoon96@users.noreply.github.com>
2022-05-23 19:00:42 +0900

    11. Ex end

--- pages/movies/[...params].js
+++ pages/movies/[...params].js
@@ -2,6 +2,7 @@ import { useRouter } from "next/router";
 
 export default function Movies() {
   const router = useRouter();
+  console.log(router);
   return (
     <div>
       <h4>{router.query.title || "로딩중"}</h4>
